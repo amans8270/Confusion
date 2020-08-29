56bfe82d0d228625ef54bb8068fef7564c3bd84b
Aman <amans8270@gmail.com>
2020-08-29 20:32:38 +0530

    Updates

---
+++ precache-manifest.8ef428c11f47bd66f4dd38a5772db2a2.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69d9222847ea6df01cad462076733517",
+    "url": "/Confusion/index.html"
+  },
+  {
+    "revision": "3e46212118bcd62af8a1",
+    "url": "/Confusion/static/css/2.90aa9c0d.chunk.css"
+  },
+  {
+    "revision": "be72f9b1c44571eaf7f7",
+    "url": "/Confusion/static/css/main.64c871f9.chunk.css"
+  },
+  {
+    "revision": "3e46212118bcd62af8a1",
+    "url": "/Confusion/static/js/2.52b19234.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/Confusion/static/js/2.52b19234.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be72f9b1c44571eaf7f7",
+    "url": "/Confusion/static/js/main.34e632ff.chunk.js"
+  },
+  {
+    "revision": "a0952c8e92e3b0e5782d",
+    "url": "/Confusion/static/js/runtime-main.ec115901.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Confusion/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Confusion/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Confusion/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Confusion/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Confusion/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
